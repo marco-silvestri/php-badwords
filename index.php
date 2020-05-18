13160d6af06e043202b49829ab8d368298ffdaf9
Marco Silvestri <m.silvestri03@gmail.com>
2020-05-18 09:45:25 +0200

    Correction

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <h1>Change the history</h1>
     <?php
     $paragraph = "Anakin, join the dark side of the force!";
-    $badword = $_GET[badword];
+    $badword = $_GET['badword'];
     ?>
     <p>Before: <strong><?php echo $paragraph; ?></strong></p>
     <p>The paragraph has a length of <?php echo strlen($paragraph); ?> characters </p>
